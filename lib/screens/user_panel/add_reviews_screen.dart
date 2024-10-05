7e2d6e5b1cc4497559d07ce2fcf77d182e3e4ab7
Hussnain <hussnain.waheed864@techservicesstudio.com>
2024-10-06 03:33:52 +0500

    Add into DB Rating bar and feedback

--- lib/screens/user_panel/add_reviews_screen.dart
+++ lib/screens/user_panel/add_reviews_screen.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:e_comm_app/models/review_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 import '../../models/order_model.dart';
@@ -17,6 +21,7 @@ class AddReviewScreen extends StatefulWidget {
 class _AddReviewScreenState extends State<AddReviewScreen> {
   TextEditingController feedbackController = TextEditingController();
   double productRating = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,7 +41,9 @@ class _AddReviewScreenState extends State<AddReviewScreen> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Text("Add Your Rating & Review"),
-            SizedBox(height: 20.0,),
+            SizedBox(
+              height: 20.0,
+            ),
             RatingBar.builder(
               initialRating: 0,
               minRating: 1,
@@ -51,29 +58,54 @@ class _AddReviewScreenState extends State<AddReviewScreen> {
               onRatingUpdate: (rating) {
                 productRating = rating;
                 print(productRating);
-                setState(() {
-
-                });
+                setState(() {});
               },
             ),
-            SizedBox(height: 20.0,),
+            SizedBox(
+              height: 20.0,
+            ),
             Text("Feedback"),
-            SizedBox(height: 20.0,),
+            SizedBox(
+              height: 20.0,
+            ),
             TextFormField(
               controller: feedbackController,
               decoration: InputDecoration(
                 label: Text("Share Your Feedback"),
               ),
             ),
-            SizedBox(height: 20.0,),
+            SizedBox(
+              height: 20.0,
+            ),
             ElevatedButton(
-                onPressed: (){
-                  String feedback =  feedbackController.text.trim();
-                  print(feedback);
-                  print(productRating);
+                onPressed: () async {
+                  EasyLoading.show(status: "Please Wait..");
+                  String feedback = feedbackController.text.trim();
+                  User? user = FirebaseAuth.instance.currentUser;
+                  // print(feedback);
+                  // print(productRating);
+
+                  ReviewModel reviewModel = ReviewModel(
+                    customerName: widget.orderModel.customerName,
+                    customerPhone: widget.orderModel.customerPhone,
+                    customerDeviceToken: widget.orderModel.customerDeviceToken,
+                    customerId: widget.orderModel.customerId,
+                    feedback: feedback,
+                    rating: productRating.toString(),
+                    createdAt: DateTime.now(),
+                  );
+
+                 await FirebaseFirestore.instance
+                      .collection('products')
+                      .doc(widget.orderModel.productId)
+                      .collection('review')
+                      .doc(user!.uid)
+                      .set(reviewModel.toMap());
+
+                 EasyLoading.dismiss();
+
                 },
                 child: Text("Save")),
-
           ],
         ),
       ),
